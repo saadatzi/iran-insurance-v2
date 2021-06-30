565afcfee463cde42be861dce3b0241cbb4b9c92
mojtaba <mojtabadelju8@gmail.com>
2021-06-30 10:49:06 +0100

    response

--- src/InsuranceType/dto/filter-insType.dto.ts
+++ src/InsuranceType/dto/filter-insType.dto.ts
@@ -1,13 +1,20 @@
-import { IsDate, IsObject, isString, IsString, MaxLength, MinLength } from "class-validator"
-import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsDate,
+  IsObject,
+  isString,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { ApiHideProperty, ApiProperty } from '@nestjs/swagger';
 
 export class FilterInsTypeDTO {
-
-    @IsString()
-    @ApiProperty({ description: 'name of the insurance type',default: 'Fire'})
-    name: string
-    
-    @ApiProperty({ type: 'string', format: 'binary'})
-    image_url: string
-
+  @IsString()
+  @ApiProperty({
+    description: 'name of the insurance type',
+    default: 'آتش سوزی',
+  })
+  name?: string;
+  @ApiProperty({ type: 'string', format: 'binary', required: false })
+  image_url: string;
 }
